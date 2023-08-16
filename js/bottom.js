8455d7efac5245478732d9e3a363655fb3e26d22
arfanur04 <arfanur04@gmail.com>
2023-08-16 08:49:00 +0600

    9-8 Create features section content and badge

--- js/bottom.js
+++ js/bottom.js
@@ -1,6 +1,6 @@
-let scroll = setInterval(function () {
-	window.scrollBy(0, document.body.scrollHeight);
-}, 2000);
+// let scroll = setInterval(function () {
+// 	window.scrollBy(0, document.body.scrollHeight);
+// }, 2000);
 
 // function scrollToBottom() {
 // 	window.scrollTo(0, document.body.scrollHeight);
