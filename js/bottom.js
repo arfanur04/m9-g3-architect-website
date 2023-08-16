dc86dfdd0d6534833450bbced72d50da8a5556b5
arfanur04 <arfanur04@gmail.com>
2023-08-16 15:51:24 +0600

    simple update

--- js/bottom.js
+++
@@ -1,9 +0,0 @@
-// let scroll = setInterval(function () {
-// 	window.scrollBy(0, document.body.scrollHeight);
-// }, 2000);
-
-// function scrollToBottom() {
-// 	window.scrollTo(0, document.body.scrollHeight);
-// }
-// history.scrollRestoration = "manual";
-// window.onload = scrollToBottom;
